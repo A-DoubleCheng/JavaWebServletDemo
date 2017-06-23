ac4714f9877110c3be41c37cf9b8cdbbb30a01ea
IT-02 <fucheng.ning@buyal.com>
2017-06-23 15:04:54 +0800

    博客main静态页面大致完成,
    在和csdn接口对接有问题，还没解决

--- src/main/java/servlet/MainServlet.java
+++ src/main/java/servlet/MainServlet.java
@@ -8,7 +8,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.net.URISyntaxException;
 import java.util.HashMap;
 
 /**
@@ -19,15 +18,12 @@ public class MainServlet extends HttpServlet {
     private final static String ACCESS_TOKEN = "9c608f2f109c4390ae40c76226440221";
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        HashMap<String, String> param = new HashMap<String, String>();
+        HashMap<String, Object> param = new HashMap<String, Object>();
         param.put("access_token", ACCESS_TOKEN);
 
-        try {
-            String s = HttpClientUtils.get("http://api.csdn.net/blog/getarticlelist", param);
+            String s = HttpClientUtils.request("GET","http://api.csdn.net/blog/getarticlelist", param);
             System.out.println(s);
-        } catch (URISyntaxException e) {
-            e.printStackTrace();
-        }
+
 
         req.getRequestDispatcher("/view/main.jsp").forward(req, resp);
     }
